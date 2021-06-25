8a0f96c92e70ed42b9873ef3dec4d18a55d411ae
AnikaGB <anika.ghosh.basu@gmail.com>
2021-06-25 13:27:19 -0700

    Stacks practice (its basically just an array)

---
+++ Stacks.py
@@ -0,0 +1,14 @@
+stack = []
+stack.append("Something")
+stack.append("that")
+stack.append("is")
+stack.append("interesting")
+
+print(str(stack) + "\n")
+print(stack.pop())
+
+print(stack)
+
+stack.append("cooooooooooooool")
+
+print(stack)
